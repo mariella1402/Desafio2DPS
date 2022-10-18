1155ee893e6023d96bdc029351328da866fd9016
mariella1402 <mariiellachiicas14@gmail.com>
2022-10-18 14:52:10 -0600

    UPDATE

--- EjerciciosExamen/Ejercicio3/Ejercicio3.js
+++ EjerciciosExamen/Ejercicio3/Ejercicio3.js
@@ -4,7 +4,7 @@ import {View, Text} from 'react-native';
 export default function About() {
   return (
     <View>
-      <Text> Estamos en About</Text>
+      <Text> TIENDA EN LINEA</Text>
     </View>
   );
 }
