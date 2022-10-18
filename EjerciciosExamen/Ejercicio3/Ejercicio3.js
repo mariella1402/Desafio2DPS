a08db005b2a4ed104a9fd4cb17e25ecf4ba759f9
mariella1402 <mariiellachiicas14@gmail.com>
2022-10-18 14:50:57 -0600

    Creacion de ejercicios

---
+++ EjerciciosExamen/Ejercicio3/Ejercicio3.js
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {View, Text} from 'react-native';
+export default function About() {
+  return (
+    <View>
+      <Text> Estamos en About</Text>
+    </View>
+  );
+}
---
+++ EjerciciosExamen/Navegacion/Navegacion.js
@@ -0,0 +1,34 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import Ejercicio1 from '../Ejercicio1/Ejercicio1';
+import Ejercicio2 from '../Ejercicio2/Ejercicio2';
+import Ejercicio3 from '../Ejercicio3/Ejercicio3';
+import Menu from '../Menu';
+const Stack = createStackNavigator();
+export default function Navigation() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Menu"
+        component={Menu}
+        options={{title: 'Menu Principal', headerTitleAlign:'center'}}
+      />
+      <Stack.Screen
+        name="Ejercicio1"
+        component={Ejercicio1}
+        options={{title: 'Ejercicio 1 (Calculadora)', headerTitleAlign:'center'}}
+      />
+      <Stack.Screen
+        name="Ejercicio2"
+        component={Ejercicio2}
+        options={{title: 'Ejercicio 2 (Conversor de Moneda)', headerTitleAlign:'center'}}
+      />
+      <Stack.Screen
+        name="Ejercicio3"
+        component={Ejercicio3}
+        options={{title: 'Ejercicio 3 (Tienda en Linea)', headerTitleAlign:'center'}}
+      />
+    </Stack.Navigator>
+  );
+}
