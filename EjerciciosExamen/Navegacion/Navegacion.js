ea5103da06bca34bb44ccf17ee83134b7da314d5
mariella1402 <mariiellachiicas14@gmail.com>
2022-10-20 11:36:10 -0600

    update ejercicio 3

--- EjerciciosExamen/Navegacion/Navegacion.js
+++ EjerciciosExamen/Navegacion/Navegacion.js
@@ -5,7 +5,15 @@ import Ejercicio1 from '../Ejercicio1/Ejercicio1';
 import Ejercicio2 from '../Ejercicio2/Ejercicio2';
 import Ejercicio3 from '../Ejercicio3/Ejercicio3';
 import Menu from '../Menu';
-import Facturacion from '../Ejercicio3/Facturacion'
+import Facturacion1 from '../Ejercicio3/Facturacion/Facturacion1';
+import Facturacion2 from '../Ejercicio3/Facturacion/Facturacion2';
+import Facturacion3 from '../Ejercicio3/Facturacion/Facturacion3';
+import Facturacion4 from '../Ejercicio3/Facturacion/Facturacion4';
+import Facturacion5 from '../Ejercicio3/Facturacion/Facturacion5';
+import Facturacion6 from '../Ejercicio3/Facturacion/Facturacion6';
+import Facturacion7 from '../Ejercicio3/Facturacion/Facturacion7';
+import Facturacion8 from '../Ejercicio3/Facturacion/Facturacion8';
+
 const Stack = createStackNavigator();
 export default function Navigation() {
   return (
@@ -31,10 +39,47 @@ export default function Navigation() {
         options={{title: 'Ejercicio 3 (Tienda en Linea)', headerTitleAlign:'center'}}
         />
         <Stack.Screen
-        name="Facturacion"
-        component={Facturacion}
+        name="Facturacion1"
+        component={Facturacion1}
         options={{title: 'Facturacion', headerTitleAlign:'center'}}
       />
+      <Stack.Screen
+        name="Facturacion2"
+        component={Facturacion2}
+        options={{title: 'Facturacion', headerTitleAlign:'center'}}
+      />
+      <Stack.Screen
+        name="Facturacion3"
+        component={Facturacion3}
+        options={{title: 'Facturacion', headerTitleAlign:'center'}}
+      />
+        <Stack.Screen
+        name="Facturacion4"
+        component={Facturacion4}
+        options={{title: 'Facturacion', headerTitleAlign:'center'}}
+      />
+       <Stack.Screen
+        name="Facturacion5"
+        component={Facturacion5}
+        options={{title: 'Facturacion', headerTitleAlign:'center'}}
+      />
+        <Stack.Screen
+        name="Facturacion6"
+        component={Facturacion6}
+        options={{title: 'Facturacion', headerTitleAlign:'center'}}
+      />
+        <Stack.Screen
+        name="Facturacion7"
+        component={Facturacion7}
+        options={{title: 'Facturacion', headerTitleAlign:'center'}}
+      />
+        <Stack.Screen
+        name="Facturacion8"
+        component={Facturacion8}
+        options={{title: 'Facturacion', headerTitleAlign:'center'}}
+      />
+
+
     </Stack.Navigator>
   );
 }
