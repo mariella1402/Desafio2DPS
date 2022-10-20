692094b8373f1b47d19181465c55e49700819b89
mariella1402 <mariiellachiicas14@gmail.com>
2022-10-20 00:31:32 -0600

    Ejercicio1

---
+++ EjerciciosExamen/Ejercicio1/Funcion.js
@@ -0,0 +1,31 @@
+/* eslint-disable prettier/prettier */
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+
+export default class CalcKeys extends Component {
+  render() {
+    return (
+      <View style={styles.calcKey}>
+        <TouchableOpacity
+          onPress={() => {
+            this.props.onClick();
+          }}>
+          <Text style={styles.textDisplay}>{this.props.displayKey}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  calcKey: {
+    backgroundColor: '#346549',
+    flex: 0.25,
+  },
+
+  textDisplay: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 36,
+  },
+});
